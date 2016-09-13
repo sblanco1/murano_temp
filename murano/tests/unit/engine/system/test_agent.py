41dac90ce42cd366b3864c37704a298908b7195f
Samantha Blanco <sblanco1@users.noreply.github.com>
2016-09-13 15:47:00 -0400

    Update test_agent.py

--- murano/tests/unit/engine/system/test_agent.py
+++ murano/tests/unit/engine/system/test_agent.py
@@ -360,3 +360,17 @@ class TestExecutionPlan(base.MuranoTestCase):
             filename = '<{0}>'.format(filename)
             body = self.agent._get_body(filename, resources, folder)
             self.assertEqual(encoded_text, body)
+
+    def test_queue_name(self):
+       self.assertEqual(self.agent.queue_name(), self.agent._queue)
+
+    def test_prepare_message(self):
+        template = {'test'}
+        msg_id = 12345
+        self.agent._prepare_message(template, msg_id)
+
+    def test_execution_plan_v1(self):
+        template = yamllib.load(
+            self._read('application.template'),
+            Loader=self.yaml_loader)
+        template = self.agent._build_v1_execution_plan(template, self.resources)
