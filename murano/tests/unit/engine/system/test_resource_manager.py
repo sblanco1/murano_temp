a71e44479b2e1c2147bed81f18a74538728ceaba
Samantha Blanco <sblanco1@users.noreply.github.com>
2016-09-13 15:49:12 -0400

    Update test_resource_manager.py

--- murano/tests/unit/engine/system/test_resource_manager.py
+++ murano/tests/unit/engine/system/test_resource_manager.py
@@ -1,4 +1,4 @@
-#    Copyright (c) 2015 Telefonica I+D
+#    Copyright (c) 2016 AT&T
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
