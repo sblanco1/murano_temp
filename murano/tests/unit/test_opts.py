43f97ac4370277da6e3267265c2a9ba57f1e293f
Samantha Blanco <sblanco1@users.noreply.github.com>
2016-09-13 15:51:54 -0400

    Create test_opts.py

---
+++ murano/tests/unit/test_opts.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2016 AT&T
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import mock
+from oslo_config import cfg
+
+from murano import opts
+from murano.tests.unit import base
+
+
+class TestOpts(base.MuranoTestCase):
+    def setUp(self):
+        super(TestOpts, self).setUp()
+        self.addCleanup(mock.patch.stopall)
+
+    def test_list_opts(self):
+        opts.list_opts()
+
+    def test_list_cfapi_opts(self):
+        opts.list_cfapi_opts()
