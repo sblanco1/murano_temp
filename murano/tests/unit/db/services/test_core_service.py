dbe611d42ca7c5794e336b39e04753ac6612ecc0
Samantha Blanco <sblanco1@users.noreply.github.com>
2016-09-28 15:11:49 -0400

    Update test_core_service.py

--- murano/tests/unit/db/services/test_core_service.py
+++ murano/tests/unit/db/services/test_core_service.py
@@ -122,3 +122,38 @@ class TestCoreServices(base.MuranoTestCase):
             self.core_services.get_template_data('any', '/services')
         self.assertEqual(fservices.applications_desc, template_des)
         template_services_mock.get_description.assert_called_with('any')
+
+    @mock.patch('murano.db.services.environment_templates.EnvTemplateServices')
+    def test_none_temp_description(self, template_services_mock):
+        fixture = self.useFixture(et.EmptyEnvironmentTemplateFixture())
+        template_services_mock.get_description.return_value = None
+        self.assertIsNone(self.core_services.
+            get_template_data('any', '/services'))
+        self.assertRaises(exc.HTTPNotFound, self.core_services.post_env_template_data,
+            'any', fixture.environment_template_desc, '/services')
+        self.assertRaises(exc.HTTPNotFound, self.core_services.post_application_data,
+            'any', fixture.environment_template_desc, '/services')
+        self.assertRaises(exc.HTTPNotFound, self.core_services.delete_env_template_data,
+            'any', '/services')
+        self.assertRaises(exc.HTTPNotFound, self.core_services.put_application_data,
+            'any', fixture.environment_template_desc, '/services')
+        
+    @mock.patch('murano.common.utils.TraverseHelper')
+    @mock.patch('murano.db.services.environments.EnvironmentServices')
+    def test_post_put_data(self, env_services_mock, source_mock):
+        fixture = self.useFixture(et.EmptyEnvironmentFixture())
+        env_services_mock.get_description.return_value = fixture.env_desc
+        source_mock = {'instances': [
+            {'id': 'ef984a74-29a4-45c0-b1dc-2ab9f075732e',
+             'type': 'io.murano.resources.LinuxInstance'},
+            {'id': 'tomcat_id', 'type': 'io.murano.apps.apache.Tomcat'},
+        ]}
+        session_id = None
+        ftomcat = self.useFixture(et.ApplicationTomcatFixture())
+        self.assertEqual(self.core_services.post_data('any', session_id,
+                         ftomcat.application_tomcat_desc,
+                         '/services'), ftomcat.application_tomcat_desc)
+
+        self.assertEqual(self.core_services.put_data('any', session_id,
+                         ftomcat.application_tomcat_desc,
+                         '/services'), ftomcat.application_tomcat_desc)
