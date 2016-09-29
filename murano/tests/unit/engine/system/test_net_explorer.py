9547880724474f85967ebb1b41d6ebfb08abeb45
Samantha Blanco <sblanco1@users.noreply.github.com>
2016-09-29 10:49:04 -0400

    Update test_net_explorer.py

--- murano/tests/unit/engine/system/test_net_explorer.py
+++ murano/tests/unit/engine/system/test_net_explorer.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import mock:q!
+import mock
 import unittest
 
 from murano.dsl import dsl
