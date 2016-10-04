9a1a588a9cd140edd9e0718d63b18f5f0a2ba6e0
Samantha Blanco <sblanco1@users.noreply.github.com>
2016-10-04 17:35:37 -0400

    Update test_environments.py

--- murano/tests/unit/db/services/test_environments.py
+++ murano/tests/unit/db/services/test_environments.py
@@ -24,37 +24,42 @@ from murano.services import states
 from murano.tests.unit import base
 from murano.tests.unit import utils
 
+OLD_VERSION = 0
+LATEST_VERSION = 1
+
 
 class TestEnvironmentServices(base.MuranoWithDBTestCase):
-    def test_environment_ready_if_last_session_deployed_after_failed(self):
-        """Test environment ready status
-
-        If last session was deployed successfully and other session
-        was failed - environment must have status "ready".
-
-        Bug: #1413260
-        """
-        OLD_VERSION = 0
-        LATEST_VERSION = 1
-
-        session = db_session.get_session()
-
-        environment = models.Environment(
+    def setUp(self):
+        super(TestEnvironmentServices, self).setUp()
+        self.environment = models.Environment(
             name='test_environment', tenant_id='test_tenant_id',
             version=LATEST_VERSION
         )
-        session.add(environment)
+
+        self.env_services = environments.EnvironmentServices()
+
+
+    def test_environment_ready_if_last_session_deployed_after_failed(self):
+        """Test environment ready status
+        If last session was deployed successfully and other session
+        was failed - environment must have status "ready".
+        Bug: #1413260
+        """
+
+        session = db_session.get_session()
+
+        session.add(self.environment)
 
         now = timeutils.utcnow()
 
         session_1 = models.Session(
-            environment=environment, user_id='test_user_id_1',
+            environment=self.environment, user_id='test_user_id_1',
             version=OLD_VERSION,
             state=states.SessionState.DEPLOY_FAILURE,
             updated=now, description={}
         )
         session_2 = models.Session(
-            environment=environment, user_id='test_user_id_2',
+            environment=self.environment, user_id='test_user_id_2',
             version=LATEST_VERSION,
             state=states.SessionState.DEPLOYED,
             updated=now + dt.timedelta(minutes=1), description={}
@@ -64,7 +69,7 @@ class TestEnvironmentServices(base.MuranoWithDBTestCase):
 
         expected_status = states.EnvironmentStatus.READY
         actual_status = environments.EnvironmentServices.get_status(
-            environment.id
+            self.environment.id
         )
 
         self.assertEqual(expected_status, actual_status)
@@ -74,25 +79,19 @@ class TestEnvironmentServices(base.MuranoWithDBTestCase):
         self.tenant_id = str(uuid.uuid4())
         self.context = utils.dummy_context(tenant_id=self.tenant_id)
 
-        driver_context = self.environments.get_network_driver(self.context)
+        driver_context = self.env_services.get_network_driver(self.context)
         self.assertEqual(driver_context, "neutron")
-        
+
     def test_get_status(self):
-        OLD_VERSION = 0
-        LATEST_VERSION = 1
 
         session = db_session.get_session()
 
-        environment = models.Environment(
-            name='test_environment', tenant_id='test_tenant_id',
-            version=LATEST_VERSION
-        )
-        session.add(environment)
+        session.add(self.environment)
 
         now = timeutils.utcnow()
 
         session_1 = models.Session(
-            environment=environment, user_id='test_user_id_1',
+            environment=self.environment, user_id='test_user_id_1',
             version=OLD_VERSION,
             state=states.SessionState.DEPLOY_FAILURE,
             updated=now, description={}
